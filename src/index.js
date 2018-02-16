605cc35a406ecf10b88ff879c6b63fe1a0046a38
Chase Ramsey <ramsey.chase@gmail.com>
2018-02-16 00:42:12 -0600

    Add footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import './main.sass'
-import Me from './me_banner_blue.png'
 
 ReactDOM.render(<App />, document.getElementById('root'));
